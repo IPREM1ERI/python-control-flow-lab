d7c3a6f73b7a61548f79fb27d3d519ace4e42e53
davidv <davidvazquezgit@gmail.com>
2022-10-31 13:09:06 -0700

    refactor problem 3 solution

--- exercise-3.py
+++ exercise-3.py
@@ -11,13 +11,11 @@
 
 # Hint:  Use the int() function to convert the string returned from input() into an integer
 
-from re import A
-
 
 dog_human_yrs = int(input("Input a dog's age in human years:"))
 
 if dog_human_yrs <= 2:
   dog_age = dog_human_yrs * 10
-elif dog_human_yrs > 2:
+else:
   dog_age = 20 + (7 * (dog_human_yrs - 2))
 print(f"The dog's age in dog years is {dog_age}")
